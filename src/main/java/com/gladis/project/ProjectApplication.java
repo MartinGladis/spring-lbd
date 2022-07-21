fe42b05603fea0e1f64f5ec935fbc62d80834de0
Martin Gladis <martin.gladis@q-perior.com>
2022-07-21 11:43:03 +0200

    Inject EmployeeService with autowired fixed with Primary annotation

--- src/main/java/com/gladis/project/ProjectApplication.java
+++ src/main/java/com/gladis/project/ProjectApplication.java
@@ -5,10 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ProjectApplication {
+
+	@Autowired
+	EmployeeService employeeService;
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+	@PostConstruct
+	public void post() {
+		employeeService.findAll();
+		System.out.println("Called");
 	}
 
 }
--- src/main/java/com/gladis/project/service/employee/EmployeeServiceImpl1.java
+++ src/main/java/com/gladis/project/service/employee/EmployeeServiceImpl1.java
@@ -1,9 +1,11 @@
 package com.gladis.project.service.employee;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Primary
 @Service
 public class EmployeeServiceImpl1 implements EmployeeService {
     public List<?> findAll() {
